834e5a5ba71dd5cb8f512d83bc09417f690f320a
Himiwari <cam.van.nguyen12@gmail.com>
2016-03-11 14:11:50 -0600

    Added More parts

--- app/src/main/java/com/example/cam/nim/Game.java
+++ app/src/main/java/com/example/cam/nim/Game.java
@@ -6,15 +6,14 @@ import android.os.Bundle;
 
 
 public class Game extends Activity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
-    public void Run()
-    {
 
-    }
 
 }
