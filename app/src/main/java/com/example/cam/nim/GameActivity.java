56f077f22e4bebe3630f9ad6fadefca7de374de2
Himiwari <cam.van.nguyen12@gmail.com>
2016-04-19 13:18:11 -0500

    Cleaning imports

--- app/src/main/java/com/example/cam/nim/GameActivity.java
+++ app/src/main/java/com/example/cam/nim/GameActivity.java
@@ -1,29 +1,22 @@
 package com.example.cam.nim;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 //import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 //import android.widget.ArrayAdapter;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 //import android.widget.ListView;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
--- app/src/main/java/com/example/cam/nim/MainMenuActivity.java
+++ app/src/main/java/com/example/cam/nim/MainMenuActivity.java
@@ -3,7 +3,7 @@ package com.example.cam.nim;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.*;
+import android.view.View;
 
 public class MainMenuActivity extends Activity {
 
