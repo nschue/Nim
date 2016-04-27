f7f800e2b02427556b94ea5bba3351bedbe11b7f
Himiwari <cam.van.nguyen12@gmail.com>
2016-04-27 14:51:01 -0500

    centering

--- app/src/main/java/com/example/cam/nim/MainMenuActivity.java
+++ app/src/main/java/com/example/cam/nim/MainMenuActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
